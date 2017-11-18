a9b047739d54f55c0693009c03162f6d92777436
Guilherme <guilhermeborgesbastos@gmail.com>
2017-11-18 18:49:06 -0200

    CollectionHelper with all the basic public methods created

--- src/main/java/me/challenge/GenericRestApplication/service/CollectionHelper.java
+++ src/main/java/me/challenge/GenericRestApplication/service/CollectionHelper.java
@@ -9,29 +9,85 @@
 package me.challenge.GenericRestApplication.service;
 
 // >> imports
+import com.mongodb.Block;
 import org.bson.Document;
 import com.mongodb.client.MongoCollection;
+import java.util.ArrayList;
+import java.util.List;
 import me.challenge.GenericRestApplication.utils.ISearch;
 import me.challenge.GenericRestApplication.utils.ValidationUtil;
 import org.bson.conversions.Bson;
 
 public class CollectionHelper {
-    	private final ISearch search;
-	private final MongoCollection<Document> collection;
-	private final ValidationUtil validation;
+    private final MongoCollection<Document> collection;
+    private final ISearch search;
+    private final ValidationUtil validation;
+    
+    // contructor
+    public CollectionHelper(MongoCollection<Document> collection, ISearch search, Document model){
+        this.collection = collection;
+        this.search = search;
+        this.validation = new ValidationUtil(model);
+    }
 
-	public CollectionHelper(MongoCollection<Document> collection, ISearch search, Document model) {
-		this.collection = collection;
-		this.search = search;
-		this.validation = new ValidationUtil(model);
-	}
-        
-        // private method(s)
-        private Bson searchByID(String ID) {
-		return search.searchByID(ID);
-	}
-        
-        // TODO: implement the public method(s)
-        
+    // public method(s)
+    /**
+     * Save a new document
+     * @param doc
+     * @return Document
+    */
+    public Document save(Document document){
+        validation.verify(document);
+        collection.insertOne(document);
+        return document;
+    }
 
+    /**
+     * 
+     * @param ID
+     * @return Document
+    */
+    public Document get(String ID){
+        return collection.find(searchByID(ID)).first();
+    }
+
+    /**
+     * Method that returns a list of all documents into Database
+     * @return List<Document>
+    */
+    public List<Document> getAll(){
+        List<Document> documents = new ArrayList<>();
+        collection.find().forEach((Block<Document>) document -> documents.add(document));
+        return documents;
+    }
+
+    /**
+     * Update a document looking for the ID field
+     * @param ID
+     * @param document
+     * @return boolean
+    */
+    public boolean update(String ID, Document document){
+        validation.verify(document);
+        return collection.replaceOne(searchByID(ID), document).wasAcknowledged();
+    }
+
+    /**
+     * Delete a document looking for the ID field
+     * @param ID
+     * @return 
+    */
+    public boolean delete(String ID){
+        return collection.deleteOne(searchByID(ID)).wasAcknowledged();
+    }
+       
+    // private method(s)
+    /**
+     * Search a document by ID
+     * @param ID
+     * @return Bson
+    */
+    private Bson searchByID(String ID){
+        return search.searchByID(ID);
+    }    
 }
