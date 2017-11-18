8f2c27498aef302ae1d9ddfca91dbed63792d94a
Guilherme <guilhermeborgesbastos@gmail.com>
2017-11-18 19:03:41 -0200

    CollectionDatabaseUtil created to take care of the management of the NoSQL Database

---
+++ src/main/java/me/challenge/GenericRestApplication/utils/CollectionDatabaseUtil.java
@@ -0,0 +1,19 @@
+/*
+ * CollectionDatabaseUtil.java
+ *
+ * Tech Test Programming Exercise Zup
+ * CollectionDatabaseUtil is the manager of the NoSQL Database
+ * @version    0.1.0
+ * @author     Guilherme Borges Bastos
+*/
+
+package me.challenge.GenericRestApplication.utils;
+
+
+public class CollectionDatabaseUtil {
+    
+    public CollectionDatabaseUtil() {
+        
+    }
+
+}
