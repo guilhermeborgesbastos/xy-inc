a0cbafd169bdd2fa0851e55d3167a2fc9dbb6e59
Guilherme <guilhermeborgesbastos@gmail.com>
2017-11-18 17:49:37 -0200

    ValidationUtil used to any field's validation in the REST( no methods yet )

---
+++ src/main/java/me/challenge/GenericRestApplication/utils/ValidationUtil.java
@@ -0,0 +1,24 @@
+/*
+ * CollectionHelper.java
+ *
+ * Tech Test Programming Exercise Zup
+ * CollectionHelper implements the auxiliary functions to manage the database
+ * @version    0.1.0
+ * @author     Guilherme Borges Bastos
+*/
+package me.challenge.GenericRestApplication.utils;
+
+// >> imports
+import org.bson.Document;
+
+public class ValidationUtil {
+    
+	private final Document model;
+
+	public ValidationUtil(Document modelo) {
+		this.model = modelo;
+	}
+
+	//TODO: finish the validation's methods
+
+}
