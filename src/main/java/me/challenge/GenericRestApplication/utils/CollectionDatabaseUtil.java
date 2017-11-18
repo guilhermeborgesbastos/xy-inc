c0cfdfe3b2ce8af883e9a4bda8189886f9832626
Guilherme <guilhermeborgesbastos@gmail.com>
2017-11-18 19:30:06 -0200

    CollectionDatabaseUtil updated with the logical part needed for the Management of MongoDB instances

--- src/main/java/me/challenge/GenericRestApplication/utils/CollectionDatabaseUtil.java
+++ src/main/java/me/challenge/GenericRestApplication/utils/CollectionDatabaseUtil.java
@@ -2,18 +2,54 @@
  * CollectionDatabaseUtil.java
  *
  * Tech Test Programming Exercise Zup
- * CollectionDatabaseUtil is the manager of the NoSQL Database
+ * CollectionDatabaseUtil is the manager of the NoSQL Database ( MongoDB )
  * @version    0.1.0
  * @author     Guilherme Borges Bastos
 */
 
 package me.challenge.GenericRestApplication.utils;
 
+// >> imports
+import org.springframework.beans.factory.annotation.Autowired;
+import org.bson.Document;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import me.challenge.GenericRestApplication.service.CollectionHelper;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CollectionDatabaseUtil {
-    
-    public CollectionDatabaseUtil() {
-        
-    }
 
+    @Autowired
+    private ISearch search;
+    
+    @Autowired
+    private MongoDatabase mongoDB;
+    
+    // Data BSON Model
+    private static final Document DATA_MODEL = Document.parse("{\"model\":\"/\",\"fields\":[{\"name\":\"model\",\"type\":\"String\",\"required\":true},{\"name\":\"fields\",\"type\":\"ArrayList\"}]}");
+    
+    /**
+     * CollectionHelper instance
+     * @param collectionName
+     * @return CollectionHelper
+    */
+    public CollectionHelper use(String collectionName) {
+        Document model = getModel(collectionName);
+        MongoCollection<Document> collection = mongoDB.getCollection(collectionName);
+        return new CollectionHelper(collection, search, model);
+    }
+    
+    /**
+     * // method to get a document searching into mongoDB's collections
+     * @param collectionName
+     * @return Document
+    */
+    private Document getModel(String collectionName) {
+        if ("/".equals(collectionName)) { return DATA_MODEL; }
+        // find the model into mongoDB
+        Document model = mongoDB.getCollection("/").find(search.searchByID(collectionName)).first();
+        if (model == null) { throw new IllegalArgumentException("Model's collection not found!"); }
+        return model;
+    }
 }
