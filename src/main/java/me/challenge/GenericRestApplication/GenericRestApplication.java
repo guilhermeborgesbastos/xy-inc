4a044452f9df9de78727e495a7b5a589ef2d7d83
Guilherme <guilhermeborgesbastos@gmail.com>
2017-11-18 15:14:41 -0200

    Initial Main Project Setup

---
+++ src/main/java/me/challenge/GenericRestApplication/GenericRestApplication.java
@@ -0,0 +1,22 @@
+/*
+ * GenericRestApplication.java
+ *
+ * Tech Test Programming Exercise Zup, 
+ * Generic Rest Application
+ * @version    0.1.0
+ * @author     Guilherme Borges Bastos
+ * @copyright  Copyright 2017 Guilherme Borges Bastos
+ * @LinkedIn   https://www.linkedin.com/in/guilhermeborgesbastos/
+*/
+package me.challenge.GenericRestApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericRestApplication.class, args);
+	}
+}
