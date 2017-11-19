bf67355204d3230284fabf5eec4520df3ecf7a7e
Guilherme <guilhermeborgesbastos@gmail.com>
2017-11-19 14:09:59 -0200

    AppConfig better formatting

--- src/main/java/me/challenge/GenericRestApplication/config/AppConfig.java
+++ src/main/java/me/challenge/GenericRestApplication/config/AppConfig.java
@@ -1,16 +1,22 @@
+/*
+ * AppConfig.java
+ *
+ * Tech Test Programming Exercise Zup
+ * Before the configuration startts this class should be loaded
+ * @version    0.1.0
+ * @author     Guilherme Borges Bastos
+*/
 package me.challenge.GenericRestApplication.config;
 
+// >> imports
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:config.properties")
 public class AppConfig {
 
     private static final String MONGODB_URI = "mongodb://mongo/";
